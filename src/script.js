657b79a3931784144f567db77e6462ae4087b422
Collin Reilly Clark <crclark96@gmail.com>
2019-07-24 11:18:31 -0400

    add filtering dropdown by township

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 var i;
 var entries = [];
+var townships = new Set();
 
 function format(source, params) {
     $.each(params,function (i, n) {
@@ -8,18 +9,40 @@ function format(source, params) {
     return source;
 }
 
-var collapsible = '<button class="collapsible">{0}</button>' +
+var collapsible = '<button class="collapsible">{0}' +
+                  '  <div align="right">{1} {2} {3}</div>' +
+                  '</button>' +
                   '<div class="content">' +
-                  '  <p>{1}</p>' +
-                  '  <p>{2}</p>' +
-                  '  <p>{3}</p>'
+                  '  <p>{4}</p>' +
+                  '  <p>{5}</p>' +
+                  '  <p>{6}</p>'
                   '</div>'
 
 blockspring.runParsed("read-worksheet-google-sheets", { "file_id": "1ekPrJc7Z9xoxihatWDXY88hS1Kj0gx2DQ0a1O0vF6oQ", "worksheet_id": "1457908608", "has_header": false}, { "api_key": "br_114955_edcef0380b1584c04100f3882e262d9f9785e92c" }, function(res){
     console.log(res.params);
+    res.params.data = res.params.data.filter(function(array){
+      return array[0] === "" ? false : true})
     for (var i = 1; i < res.params.data.length; i++) {
-      $("body").append(format(collapsible,res.params.data[i]))
+      var data = res.params.data[i];
+      var content = [data[0],
+                    (data[1] === "Sp" ? "🇲🇽" : "🇺🇸"),
+                    (data[2] === "Yes" ? "🛂" : "✔"),
+                    (data[3] === "Free" ? "" : "💵"),
+                    data[4], data[5], data[6]]
+      townships.add(data[4]);
+      entries.push(content)
     }
+    townships.forEach(function(val) {
+      $("#township").append(format('<option value="{0}">{0}</option>',[val]));
+      $("#township").children().last().click(function() {
+        rel_entries = entries.filter(function(entry){
+          return entry[4] === val;
+        });
+        rel_entries.forEach(function(entry){
+          $("body").append(format(collapsible,entry))
+        });
+      });
+    });
     $(".collapsible").click(function() {
       this.classList.toggle("active");
       var content = this.nextElementSibling;
