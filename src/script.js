3f52c5f74a3bef0e7d6d724613af2bb71f83fd6f
Collin Reilly Clark <crclark96@gmail.com>
2019-08-01 10:51:56 -0400

    change to a categories screen, #nofilter

--- src/script.js
+++ src/script.js
@@ -1,72 +1,70 @@
 var i;
 var entries = [];
 var rel_entries = [];
-var townships = new Set();
 var categories = new Set();
 
 function format(source, params) {
-    $.each(params,function (i, n) {
-        source = source.replace(new RegExp("\\{" + i + "\\}", "g"), n);
-    })
-    return source;
+  $.each(params,function (i, n) {
+    source = source.replace(new RegExp("\\{" + i + "\\}", "g"), n);
+  })
+  return source;
 }
 
-function createDropdown(dropdown, val) {
-  console.log(dropdown);
-  console.log(val);
-  $("#" + dropdown).append(format('<option value="{0}">{0}</option>',[val]));
-  $("#" + dropdown).change(function() {
-    $(".collapsible").remove();
-    $(".content").remove();
+function categoriesScreen() {
+  for (var i = 0; i < categories.size; i++) {
+    var categoryButton = '<button class="categoryButton">{0}</button>';
+    $("body").append(format(categoryButton,[Array.from(categories)[i]]));
+  }
+  $(".categoryButton").on('click', function() {
+    var category = this.innerText;
+    $(".categoryButton").remove();
+    $("body").append(backButton);
+    $(".backButton").on('click', function(){
+      $("body").children().remove();
+      categoriesScreen();
+    });
     rel_entries = entries.filter(function(entry){
-      return (entry[5] === $("#township").val()
-              && entry[0] === $("#category").val());
+      return entry[0] == category;
     });
     rel_entries.forEach(function(entry){
-      $("body").append(format(collapsible,entry))
+      $("body").append(format(collapsible,entry));
     });
-    $(".collapsible").on('click touchstart', function() {
+    $(".collapsible").on('click', function(){
       this.classList.toggle("active");
       var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
       } else {
-        content.style.display = "block";
+        content.style.display = 'block';
       }
     });
   });
 }
 
-var collapsible = '<button class="collapsible">{1} - {6}' +
-                  '  <div align="right">{2} {3} {4}</div>' +
+var collapsible = '<button class="collapsible">{5} - {4}' +
+                  '  <div align="right">{1} {2} {3}</div>' +
                   '</button>' +
                   '<div class="content">' +
-                  '  <p><a href="tel:{7}">{7}</a></p>' +
+                  '  <p><a href="tel:{6}">{6}</a></p>' +
+                  '  <p>{7}</p>' +
                   '  <p>{8}</p>' +
                   '  <p>{9}</p>' +
                   '  <p>{10}</p>'+
                   '  <p>{11}</p>'+
-                  '</div>'
+                  '</div>';
 
-blockspring.runParsed("read-worksheet-google-sheets", { "file_id": "1ekPrJc7Z9xoxihatWDXY88hS1Kj0gx2DQ0a1O0vF6oQ", "worksheet_id": "1457908608", "has_header": false}, { "api_key": "br_114955_edcef0380b1584c04100f3882e262d9f9785e92c" }, function(res){
+var backButton = '<button class="backButton">\<-- Back</button>';
+
+blockspring.runParsed("read-worksheet-google-sheets", { "file_id": "1sPTrQGlMw7bZeuOxK9WZWazfpi-IYKFqSN-62wga3ww", "worksheet_id": "1878156380", "has_header": false}, { "api_key": "br_114955_edcef0380b1584c04100f3882e262d9f9785e92c" }, function(res){
   console.log(res.params);
   res.params.data = res.params.data.filter(function(array){
     return array[0] === "" ? false : true})
   for (var i = 1; i < res.params.data.length; i++) {
-    var data = res.params.data[i];
-    var content = [data[0], data[1],
-                   (data[2] === "Sp" ? "sp" : "en"),
-                   (data[3] === "Yes" ? "🛂" : "✔"),
-                   (data[4] === "Free" ? "free" : "💵"),
-                   data[5], data[6], data[7], data[8], data[9], data[10], data[11]]
-    townships.add(data[5]);
-    categories.add(data[0]);
-    entries.push(content)
+    var content = res.params.data[i];
+    categories.add(content[0]);
+    entries.push(content);
   }
-  for (var i = 0; i < townships.size; i++) {
-    createDropdown("township",Array.from(townships)[i]);
-  }
-  for (var i = 0; i < categories.size; i++) {
-    createDropdown("category",Array.from(categories)[i]);
-  }
+  categoriesScreen();
+  
+  
 });
